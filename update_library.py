e7996a8bd7a8e5d9f347daf85dca69ba1f665fea
James Adams <jrha@Octane>
2013-01-12 13:17:48 +0000

    Add argument parser and clean up.

--- update_library.py
+++ update_library.py
@@ -8,20 +8,24 @@ from library import *
 
 mimetypes.init()
 
+import argparse
+parser = argparse.ArgumentParser(description='Super Simple Player - Library Updater')
+parser.add_argument('path', help="Path of the root of your music collection")
+args = parser.parse_args()
+del parser
+
 session = connect()
 
-TOP = u"/home/media/audio/ReTagged/"
+if args.path:
+    for root, dirs, files in os.walk(args.path):
+        for f in files:
+            filepath = "%s/%s" % (root, f)
+            mimetype = mimetypes.guess_type(filepath)
 
-for root, dirs, files in os.walk(TOP):
-  for f in files:
-      filepath = "%s/%s" % (root, f)
-      mimetype = mimetypes.guess_type(filepath)
-      if type(mimetype) is tuple:
-          mimetype = mimetype[0]
-      if mimetype and "audio" in mimetype:
-          #print("+ %-20s%s " % (mimetype, filepath))
-          session.add(sspTrack(filepath))
-      #else:
-          #print("  %-20s%s" % (mimetype, filepath))
+            if type(mimetype) is tuple:
+                mimetype = mimetype[0]
+
+            if mimetype and "audio" in mimetype:
+                session.add(sspTrack(filepath))
 
 session.commit()
