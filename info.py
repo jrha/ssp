384abca707c47f1ff0accc48ac9beff1c53185c5
James Adams <james@bluezen.co.uk>
2013-06-24 00:23:38 +0100

    Add informational script
    This contains helper functions for inspecting usage and listening habits.

---
+++ info.py
@@ -0,0 +1,89 @@
+#!/usr/bin/env python2
+# -*- coding: utf-8 -*-
+#
+#  Copyright (C) 2013  James Adams
+#
+#  This program is free software; you can redistribute it and/or modify
+#  it under the terms of the GNU General Public License as published by
+#  the Free Software Foundation; either version 2 of the License, or
+#  (at your option) any later version.
+#
+#  This program is distributed in the hope that it will be useful,
+#  but WITHOUT ANY WARRANTY; without even the implied warranty of
+#  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#  GNU General Public License for more details.
+#
+#  You should have received a copy of the GNU General Public License
+#  along with this program; if not, write to the Free Software
+#  Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston,
+#  MA 02110-1301, USA.
+#
+
+from library import *
+
+from prettytable import PrettyTable
+
+
+def last_played(library):
+    tracks = library.query(sspTrack).order_by(sspTrack.lastplayed.desc()).limit(50).all()
+    fields = ["Last Played", "Artist", "Track"]
+    table = PrettyTable(fields)
+
+    for field in fields:
+        table.set_field_align(field, "l")
+
+    for track in tracks:
+        filepath = track.filepath.split("/")
+        table.add_row([track.lastplayed, filepath[-3], filepath[-1]])
+
+    table.printt()
+
+
+def most_skipped_artists(library):
+    tracks = library.query(sspTrack.filepath).filter(sspTrack.skipcount >= 1).all()
+
+    fields = ["Skips", "Artist"]
+    table = PrettyTable(fields)
+
+    for field in fields:
+        table.set_field_align(field, "l")
+
+    artists = {}
+
+    for track in tracks:
+        artist = track.filepath.split("/")[-3]
+        if artist not in artists:
+            artists[artist] = 0
+        artists[artist] += 1
+
+    artists = [ [s,a] for a,s in artists.iteritems() ]
+    artists.sort(key = lambda record: record[1])
+    artists.sort(key = lambda record: record[0])
+    artists.reverse()
+
+    for record in artists[:20]:
+        table.add_row(record)
+
+    table.printt()
+
+
+def playthrough_progress(library):
+    stats = library.query(func.count(sspTrack.playcount)).group_by(sspTrack.playcount).order_by(sspTrack.playcount.desc()).all()
+    played = float(stats[0][0])
+    unplayed = float(stats[1][0])
+    print "Currently %.1f%% through current pass of library" % (played / (played + unplayed) * 100)
+
+
+if __name__ == "__main__":
+    library = connect()
+    import argparse
+    parser = argparse.ArgumentParser(description='Super Simple Player')
+    parser.add_argument('info', choices=["last_played", "most_skipped_artists", "playthrough_progress"], help="Which piece of information to display")
+    args = parser.parse_args()
+    
+    if args.info == "last_played":
+        last_played(library)
+    elif args.info == "most_skipped_artists":
+        most_skipped_artists(library)
+    elif args.info == "playthrough_progress":
+        playthrough_progress(library)
