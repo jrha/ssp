6f0a9a5db221e8f505adb080905642b709898404
James Adams <james@bluezen.co.uk>
2013-08-22 23:11:56 +0100

    Rework info script to provide a web console

--- info.py
+++ info.py
@@ -21,13 +21,74 @@
 
 from time import sleep
 from sys import stdout
+import web
 
 from library import *
 
 from prettytable import PrettyTable
 
+URLS = (
+    '/(.*)', 'info',
+)
 
-def last_played(library):
+PAGES = [
+    'last_played',
+    'most_skipped_artists',
+    'playthrough_progress',
+    'unplay_last_played',
+    'stats',
+]
+
+render = web.template.render('templates')
+
+def navbar(page):
+    result = []
+    for p in PAGES:
+        active = ''
+        if p == page:
+            active = ' class="active"'
+        result.append('<li%s><a href="%s">%s</a></li>' % (active, p, p.replace('_', ' ').title()))
+    return "\n".join(result)
+
+
+def pageify(page, content):
+    #template = open("template.html").read().decode('utf-8')
+    nav = navbar(page)
+    title = page.replace('_', ' ').title()
+    return render.index(nav, title, content)
+
+
+def htmlify(page, table):
+    return pageify(page, table.get_html_string(border=False, format=False, attributes={'class' : 'table table-striped table-hover'}))
+
+
+class info():
+    def GET(self, name):
+        if name == "last_played":
+            return last_played()
+        elif name == "unplay_last_played":
+            return unplay_last_played()
+        elif name == "most_skipped_artists":
+            return most_skipped_artists()
+        elif name == "playthrough_progress":
+            return playthrough_progress()
+        elif name == "stats":
+            return stats()
+        else:
+            return home()
+
+
+def home():
+    library = connect()
+    return pageify(
+        'Super Simple Player',
+        '<p>Schema Version %s</p>' % SCHEMA_VERSION +
+        '<p>%d tracks in library</p>' % library.query(sspTrack).count()
+    )
+
+
+def last_played():
+    library = connect()
     tracks = library.query(sspTrack).order_by(sspTrack.lastplayed.desc()).limit(50).all()
     fields = ["Last Played", "Artist", "Track"]
     table = PrettyTable(fields)
@@ -39,29 +100,26 @@ def last_played(library):
         filepath = track.filepath.split("/")
         table.add_row([track.lastplayed, filepath[-3], filepath[-1]])
 
-    table.printt()
+    return htmlify('last_played', table)
 
 
-def unplay_last_played(library):
+def unplay_last_played():
+    library = connect()
     track = library.query(sspTrack).order_by(sspTrack.lastplayed.desc()).first()
-    print "Unplaying: %s" % (track.filepath)
-    print "          plays:%d" % (track.playcount)
-    print "          skips: %d" % (track.skipcount)
-    print "    last played: %s" % (track.lastplayed)
-    print
-    print "This will only re-queue an accidentally played track, hourly play statistics will be unaffected."
-    print "There will be a five second pause while you think about what you have done."
-    for i in range(0, 5):
-        stdout.write("%d..." % (5-i))
-        stdout.flush()
-        sleep(1)
-    track.playcount -= 1
-    track.lastplayed = None
-    library.commit()
-    print "DONE"
+
+    return render.unplay_last_played(navbar('unplay_last_played'), track.filepath, track.playcount, track.skipcount, track.lastplayed)
+    #for i in range(0, 5):
+    #    stdout.write("%d..." % (5-i))
+    #    stdout.flush()
+    #    sleep(1)
+    #track.playcount -= 1
+    #track.lastplayed = None
+    #library.commit()
+    #print "DONE"
 
 
-def most_skipped_artists(library):
+def most_skipped_artists():
+    library = connect()
     tracks = library.query(sspTrack.filepath).filter(sspTrack.skipcount >= 1).all()
 
     fields = ["Skips", "Artist"]
@@ -86,28 +144,44 @@ def most_skipped_artists(library):
     for record in artists[:20]:
         table.add_row(record)
 
-    table.printt()
+    return htmlify('most_skipped_artists', table)
 
 
-def playthrough_progress(library):
+def playthrough_progress():
+    library = connect()
     stats = library.query(func.count(sspTrack.playcount)).group_by(sspTrack.playcount).order_by(sspTrack.playcount.desc()).all()
     played = float(stats[0][0])
     unplayed = float(stats[1][0])
-    print "Currently %.1f%% through current pass of library" % (played / (played + unplayed) * 100)
+    perc = '%.1f%%' % (played / (played + unplayed) * 100)
+    return render.playthrough_progress(navbar('playthrough_progress'), perc)
+
+
+def stats():
+    library = connect()
+    rawstats = library.query(sspStat).all()
+
+    limit = float(max(
+        max([s.playcount for s in rawstats]),
+        max([s.skipcount for s in rawstats])
+    ))
+
+    stats = []
+
+    for s in rawstats:
+        stats.append(
+            (
+                '%.2d - %.2d' % (s.hour, s.hour+1),
+                '%.1f%%' % (50 - (s.skipcount / limit) * 50),
+                '%.1f%%' % ((s.skipcount / limit) * 50),
+                '%.1f%%' % ((s.playcount / limit) * 50),
+                s.skipcount,
+                s.playcount,
+            )
+        )
+
+    return render.stats(navbar('stats'), limit, stats)
 
 
 if __name__ == "__main__":
-    library = connect()
-    import argparse
-    parser = argparse.ArgumentParser(description='Super Simple Player')
-    parser.add_argument('info', choices=["last_played", "unplay_last_played", "most_skipped_artists", "playthrough_progress"], help="Which piece of information to display")
-    args = parser.parse_args()
-
-    if args.info == "last_played":
-        last_played(library)
-    elif args.info == "unplay_last_played":
-        unplay_last_played(library)
-    elif args.info == "most_skipped_artists":
-        most_skipped_artists(library)
-    elif args.info == "playthrough_progress":
-        playthrough_progress(library)
+    app = web.application(URLS, globals())
+    app.run()
